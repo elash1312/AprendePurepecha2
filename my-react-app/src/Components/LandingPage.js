762829977977810400a6ff422157e9e07b86bc8f
Erin Lash <95651459+elash1312@users.noreply.github.com>
2024-02-09 04:13:54 +0000

    landing page edits

--- my-react-app/src/Components/LandingPage.js
+++ my-react-app/src/Components/LandingPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './LandingPage.css';
 
 // Landing page component
 function LandingPage() {
   return (
     <div className="landing-page">
       <header>
-        <h1>¡Bienvenidos a Aprende Purépecha!</h1>
+      <h1 style={{ color: 'white' }}>¡Bienvenidos a Aprende Purépecha!</h1>
       </header>
       <section className="hero">
-        <h2>Un espacio para aprender purépecha,</h2>
-        <h2> por la preservación de nuestra hermosa cultura e idioma.</h2>
+        <h2 style={{ color: 'white' }}>Un espacio para aprender purépecha,</h2>
+        <h2 style={{ color: 'white' }}> por la preservación de nuestra hermosa cultura e idioma.</h2>
         <h3>Tarjetas didácticas</h3>
         <button>Get Started</button>
       </section>
