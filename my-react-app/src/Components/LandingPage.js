5edc9594c3d59cea10158dbdf20c6e541ceeca66
Erin Lash <95651459+elash1312@users.noreply.github.com>
2024-02-09 03:24:08 +0000

    first commit- app created, landing page created

---
+++ my-react-app/src/Components/LandingPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// Landing page component
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <header>
+        <h1>¡Bienvenidos a Aprende Purépecha!</h1>
+      </header>
+      <section className="hero">
+        <h2>Un espacio para aprender purépecha,</h2>
+        <h2> por la preservación de nuestra hermosa cultura e idioma.</h2>
+        <h3>Tarjetas didácticas</h3>
+        <button>Get Started</button>
+      </section>
+      <section className="about">
+        <h2>About Us</h2>
+        <p>We are dedicated to providing the best learning experience.</p>
+      </section>
+      <footer>
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default LandingPage;
+
+
+
